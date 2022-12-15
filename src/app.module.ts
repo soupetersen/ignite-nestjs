d944632bdf2d489d2cf58fae356f733e6c022036
soupetersen <soupetersen@gmail.com>
2022-12-15 20:25:06 -0300

    feat: Add kafka for communication of microservices

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { SendNotification } from "./app/use-cases/send-notification";
 
 import { DatabaseModule } from "./infra/database/database.module";
 import { HttpModule } from "./infra/http/http.module";
+import { MessagingModulee } from "./infra/messaging/messaging.module";
 
 @Module({
-    imports: [HttpModule, DatabaseModule],
+    imports: [HttpModule, DatabaseModule, MessagingModulee],
     providers: [SendNotification],
 })
 export class AppModule {}
